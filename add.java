bfe1c67955bcb76440af710abeb4731d175a90ba
ShreyaRajalbandi <rajalabandishreya2005@gmail.com>
2025-04-09 14:25:10 +0530

    we r adding

--- add.java
+++ add.java
@@ -2,7 +2,8 @@ public class add {
     public static void main(String[]args){
         int a=5;
         int b=10;
-        int sum =a+b;
+        int c=10;
+        int sum =a+b+c;
         System.out.println("The sum is : "+sum);
     }
 }
